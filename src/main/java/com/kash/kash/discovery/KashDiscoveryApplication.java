e8e03d95403f65f8958b0da02d5cfb450f1de8fe
Joel George <joelngeorge@outlook.com>
2021-05-30 16:28:27 +0530

    initial commit

---
+++ src/main/java/com/kash/kash/discovery/KashDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.kash.kash.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kash/kash/discovery/KashDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kash.kash.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
